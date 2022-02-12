a4e5688cea6ced1a2df8ef54fa56fc1d57a680ad
SharadSaha <76741905+SharadSaha@users.noreply.github.com>
2022-02-12 21:27:35 +0530

    Update Model.py
    Updated Model.py

--- model/Model.py
+++ model/Model.py
@@ -1,4 +1,3 @@
-
 import pandas as pd
 import numpy as np
 import pickle
@@ -23,11 +22,11 @@ def get_symp_hash():
   return symp_hash
 
 def label_encoding(dataset):
-    for i in disease_hash:
-        df['Disease'] = df['Disease'].replace(i,disease_hash[i])
-    y = df['Disease']
-    df.drop(['Disease'],axis=1,inplace=True)
-    return y
+  column = []
+  for i in range(len(dataset)):
+    disease = dataset['Disease'].iloc[i]
+    column.append(disease_hash[disease])
+  return column
 
 def extract_information(disease):
     desc_index = symp_Description.loc[symp_Description['Disease']==disease]['Description']._index[0]
@@ -39,50 +38,70 @@ def extract_information(disease):
             precautions.append(symp_precaution.loc[symp_precaution['Disease']==disease][i][desc_index])
     return desc['Description'][desc_index],precautions[1:len(precautions)-1]
 
-if __name__ == '__main__':
-    dataset = pd.read_csv("dataset.csv")
-    symp_severity = pd.read_csv("Symptom-severity.csv")
-    symp_Description = pd.read_csv("symptom_Description.csv")
-    symp_precaution = pd.read_csv("symptom_precaution.csv")
 
-    symp_Description['Index'] = pd.Series([a for a in range(len(symp_Description))])
-    symp_severity['Index'] = pd.Series([a for a in range(len(symp_severity))])
-    symp_precaution['Index'] = pd.Series([a for a in range(len(symp_precaution))])
+dataset = pd.read_csv("dataset.csv")
+symp_severity = pd.read_csv("Symptom-severity.csv")
+symp_Description = pd.read_csv("symptom_Description.csv")
+symp_precaution = pd.read_csv("symptom_precaution.csv")
 
-    disease_hash = create_disease_hash()
+symp_Description['Index'] = pd.Series([a for a in range(len(symp_Description))])
+symp_severity['Index'] = pd.Series([a for a in range(len(symp_severity))])
+symp_precaution['Index'] = pd.Series([a for a in range(len(symp_precaution))])
 
-    symp_hash = get_symp_hash()
+disease_hash = create_disease_hash()
 
-    symptoms = []
-    for i in symp_severity['Symptom']:
-        if i not in symptoms:
-            symptoms.append(i)
-            
-    symptoms.append('Disease')
-    df = pd.DataFrame(columns = symptoms,data = np.zeros(dtype=int,shape=(len(dataset),len(symptoms))))
+symp_hash = get_symp_hash()
 
-    for i in range(len(dataset)):
-      record = dataset.iloc[i]
-      for col in dataset.columns:
-            if col != 'Disease':
-                if str(record[col]).replace(' ','') != 'nan':
-                    column = str(record[col].replace(' ',''))
-                    df.loc[i,column] = 1
+symptoms = []
+for i in symp_severity['Symptom']:
+    if i not in symptoms:
+        symptoms.append(i)
+        
+symptoms.append('Disease')
+df = pd.DataFrame(columns = symptoms,data = np.zeros(dtype=int,shape=(len(dataset),len(symptoms))))
+
+for i in range(len(dataset)):
+  record = dataset.iloc[i]
+  for col in dataset.columns:
+        if col != 'Disease':
+            if str(record[col]).replace(' ','') != 'nan':
+                column = str(record[col].replace(' ',''))
+                df.loc[i,column] = 1
+
+df.drop('Disease',axis=1,inplace=True)
+y = label_encoding(dataset)
+
+train_data,test_data,train_label,test_label = train_test_split(df.values,y,test_size=0.2,shuffle=True)
+
+rf = RandomForestClassifier()
+rf.fit(train_data,train_label)
+
+
+pkl_filename = "rf.h5"
+with open(pkl_filename, 'wb') as file:
+    pickle.dump(rf, file)
+
+with open(pkl_filename, 'rb') as file:
+    pickle_model = pickle.load(file)    
+
+# Calculate the accuracy score and predict target values
+score = pickle_model.score(test_data, test_label)
+print("Test score: {0:.2f} %".format(100 * score))
+
+
+def load_model(path):
+  with open(path, 'rb') as file:
+    pickle_model = pickle.load(file)
+  return pickle_model
+  
+def predict(model,data):
+  d = np.zeros(shape=(1,len(symp_hash)))
+
+  for i in range(len(data)):
+    d[0][symp_hash[data[i]]] = 1		
+  return (m.predict(d))	
     
-    y = label_encoding(df)
-    train_data,test_data,train_label,test_label = train_test_split(df,y,test_size=0.2,shuffle=True)
+l = ['vomiting','headache',]
+m = load_model('rf.h5')
 
-    rf = RandomForestClassifier()
-    rf.fit(train_data,train_label)
-
-
-    pkl_filename = "rf.pkl"
-    with open(pkl_filename, 'wb') as file:
-        pickle.dump(rf, file)
-
-    with open(pkl_filename, 'rb') as file:
-        pickle_model = pickle.load(file)    
-
-    # Calculate the accuracy score and predict target values
-    score = pickle_model.score(test_data, test_label)
-    print("Test score: {0:.2f} %".format(100 * score))
+print(predict(m,l))
