b06e56f0bff10c7c99ffc8ff1dab3253df928fe6
Pratyan Majumder <heypratyan@gmail.com>
2022-02-13 02:09:59 +0530

    Update self_diagnosis.py
    Added Web3, Upload button

--- apps/self_diagnosis.py
+++ apps/self_diagnosis.py
@@ -3,6 +3,22 @@ from . import login
 import pickle
 import pandas as pd
 import numpy as np
+from web3 import Web3
+
+#for connecting to Ganache
+w3 = Web3(Web3.HTTPProvider("HTTP://127.0.0.1:8545"))
+chain_id = 1337
+my_address = "0x90F8bf6A479f320ead074411a4B0e7944Ea8c9C1"
+private_key = '0x4f3edf983ac636a65a842ce7c78d9aa706d3b113bce9c46f30d7d21715b23b1d'
+nonce = w3.eth.getTransactionCount(my_address) -1 #Basically the number of transaction by the curent accnt
+
+#contract object
+contract_address = '0xe78A0F7E598Cc8b0Bb87894B0F60dD2a88d6a8Ab'
+contract_abi = [{'inputs': [], 'stateMutability': 'nonpayable', 'type': 'constructor'}, {'inputs': [], 'name': 'breakAccess', 'outputs': [], 'stateMutability': 'nonpayable', 'type': 'function'}, {'inputs': [{'internalType': 'address', 'name': '_doctor', 'type': 'address'}], 'name': 'breakAccessByPatient', 'outputs': [], 'stateMutability': 'nonpayable', 'type': 'function'}, {'inputs': [{'internalType': 'address', 'name': '_newHead', 'type': 'address'}], 'name': 'changeHead', 'outputs': [], 'stateMutability': 'nonpayable', 'type': 'function'}, {'inputs': [{'internalType': 'address', 'name': '_doctor', 'type': 'address'}], 'name': 'giveAccess', 'outputs': [], 'stateMutability': 'nonpayable', 'type': 'function'}, {'inputs': [{'internalType': 'address', 'name': '', 'type': 'address'}, {'internalType': 'uint256', 'name': '', 'type': 'uint256'}], 'name': 'ledger', 'outputs': [{'internalType': 'string', 'name': 'name', 'type': 'string'}, {'internalType': 'address', 'name': 'patientAddress', 'type': 'address'}, {'internalType': 'string', 'name': 'prediction', 'type': 'string'}], 'stateMutability': 'view', 'type': 'function'}, {'inputs': [], 'name': 'recieveByDoctor', 'outputs': [{'components': [{'internalType': 'string', 'name': 'name', 'type': 'string'}, {'internalType': 'address', 'name': 'patientAddress', 'type': 'address'}, {'internalType': 'string[]', 'name': 'symptoms', 'type': 'string[]'}, {'internalType': 'string', 'name': 'prediction', 'type': 'string'}], 'internalType': 'struct Patient_Data.Report[]', 'name': '', 'type': 'tuple[]'}], 'stateMutability': 'view', 'type': 'function'}, {'inputs': [], 'name': 'recieveByPatient', 'outputs': [{'components': [{'internalType': 'string', 'name': 'name', 'type': 'string'}, {'internalType': 'address', 'name': 'patientAddress', 'type': 'address'}, {'internalType': 'string[]', 'name': 'symptoms', 'type': 'string[]'}, {'internalType': 'string', 'name': 'prediction', 'type': 'string'}], 'internalType': 'struct Patient_Data.Report[]', 'name': '', 'type': 'tuple[]'}], 'stateMutability': 'view', 'type': 'function'}, {'inputs': [{'internalType': 'string', 'name': '_name', 'type': 'string'}, {'internalType': 'string[]', 'name': '_symptoms', 'type': 'string[]'}, {'internalType': 'string', 'name': '_pridiction', 'type': 'string'}], 'name': 'upload', 'outputs': [], 'stateMutability': 'nonpayable', 'type': 'function'}]
+
+contract = w3.eth.contract(address = contract_address, abi=contract_abi) # contract object after the transaction
+
+
 
 def get_symp_hash():
     # symp_severity = pd.read_csv('C:/Users/Sharad/Desktop/files/ML/test_hack/model/Symptom-severity.csv')
@@ -37,6 +53,21 @@ def create_disease_hash():
         get_disease[x] = disease_name[x]
     return disease_hash,get_disease
 
+#Upload button
+def Upload(name, symptoms, prediction):
+
+    #1
+    store_transaction = contract.functions.upload(name, symptoms, prediction).buildTransaction(
+            {"chainId":chain_id, "from":my_address, "nonce":nonce+1}
+        ) #"nonce" =+1 since after the declaration of the variable we have done one transaction
+    #2
+    signed_store_transaction = w3.eth.account.sign_transaction(store_transaction, private_key=private_key)
+    #3
+    send_store_transaction = w3.eth.sendRawTransaction(signed_store_transaction.rawTransaction)
+    # wait for the complition of the transaction
+    transaction_receipt = w3.eth.waitForTransactionReceipt(send_store_transaction)
+
+
 
 def render_diagnosis_page():
     m = '<h1 style="text-align:center">SELF DIAGNOSIS</h1>'
@@ -48,6 +79,7 @@ def render_diagnosis_page():
     n=len(symptoms)
 
     # symp_hash = Model.get_symp_hash()
+
 
     checks = {symptom:False for symptom in symptoms}
     col1,col2,col3 = st.columns(3)
@@ -68,9 +100,13 @@ def render_diagnosis_page():
             data.append(symptom)
     if len(data)>0:
         disease_hash,get_disease = create_disease_hash()
-        model = load_model('C:/Users/Sharad/Desktop/files/ML/test_hack/apps/rf.h5')
+        model = load_model('./apps/rf.h5')
         st.header("You have entered the following symptoms: {}".format(",".join(data)))
         st.header("Your may have: {}".format(get_disease[predict(model,data)[0]]))
+
+        #upload button
+        if st.button('Upload'):
+             Upload("Pratyan Majumder", ["Cough", "Cold"], "Cold")
     
 
 def app():
