d56bf721f14690bd044829fa936be734a9023171
SharadSaha <76741905+SharadSaha@users.noreply.github.com>
2022-02-13 13:17:32 +0530

    Update appointments.py

--- apps/appointments.py
+++ apps/appointments.py
@@ -2,6 +2,8 @@ import streamlit as st
 from . import login,db
 
 def app():
+    date = ""
+    time =""
     logged_in_as_doctor,logged_in_as_patient,username = login.app()
     if logged_in_as_patient:
         doctorname = db.get_doctorname(username)
@@ -9,6 +11,9 @@ def app():
             st.header("You have no appointments yet")
         else:
             st.header("You have an appointment with Dr. {}".format(doctorname))
+            date,time = db.get_appointment_patient(username)
+            st.subheader("Date : {}".format(date))
+            st.subheader("Time : {}".format(time))
     elif logged_in_as_doctor:
         st.warning("You are not logged in as patient!")
     else:
